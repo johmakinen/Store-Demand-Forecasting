96c27165b3d61d452466d2d9050d3a2d3cfc6385
JiiMakinen <44932698+JiiMakinen@users.noreply.github.com>
2019-02-09 22:51:31 +0200

    Imported packages and data, formatted datetime

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+
+color = sns.color_palette()
+sns.set_style('darkgrid')
+
+# Import data from csv
+train = pd.read_csv('train.csv')
+
+# Format datetime
+train['date'] = pd.to_datetime(train['date'], format="%Y-%m-%d")
+
+# Take a look at the data
+print(train.head(10))
+
+
 
