7510954f518444cb7b778a9366b0edb4f61b1414
JiiMakinen <44932698+JiiMakinen@users.noreply.github.com>
2019-02-14 17:21:09 +0200

    Refining file's readability

--- errors.py
+++ errors.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+# Define a function to calculate the symmetric and non-symmetric mean absolute percentage errors
 
 def errors(true_values, predicted_values):
     mape = np.mean(abs((true_values-predicted_values)/true_values))*100
     smape = np.mean((np.abs(predicted_values - true_values) * 200/ (np.abs(predicted_values) + np.abs(true_values))).fillna(0))
     print('MAPE: %.2f %% \nSMAPE: %.2f' % (mape, smape), "%")
+
